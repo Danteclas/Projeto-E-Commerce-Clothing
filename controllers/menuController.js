809a22a7ff22a8cc14c239ecdf79d2272cf26bad
Daniel <daniteclas2014@gmail.com>
2022-05-18 18:46:35 -0300

    upadate

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,6 +1,16 @@
 const menuController={
   getHomepage:(req,res)=>{
      res.render('index');
-  }
+  },
+ /* getProduct: (req,res)=>{
+    res.render('product')
+  },
+  getSearch :(req,res)=> {
+    res.render('search')
+  },
+ /* getSearch :(req,res)=> {
+    res.render('404')
+  },*/
 }
+
 module.exports = menuController;
--- routes/menu.js
+++ routes/menu.js
@@ -4,5 +4,8 @@ const menuRouter = express.Router();
 const menuController = require('../controllers/menuController'); 
    
 menuRouter.get('/',menuController.getHomepage);
+//menuRouter.get('/product/:id',menuController.getProduct);
+//menuRouter.get('/search/:id',menuController.getSearch);
+//menuRouter.get('/404/:id',menuController.get404);
 
 module.exports = menuRouter;
